ec0f1e4275113cb8eea6b83d2579acd7db1c6730
Xinyu Wu <xinyuwu264@gmail.com>
2025-03-30 15:09:34 -0500

    clear bss

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,22 @@ mod sbi;
 
 core::arch::global_asm!(include_str!("entry.asm"));
 
+// TODO: understand the syntax
+// clear BSS segment
+fn clear_bss() {
+    unsafe extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8). write_volatile(0)}
+    });
+}
+
 #[unsafe(no_mangle)]
 fn rust_main() {
     println!("Hello, world!");
     crate::syscalls::sys_exit(9);
+    clear_bss();
     crate::sbi::shutdown();
 }
